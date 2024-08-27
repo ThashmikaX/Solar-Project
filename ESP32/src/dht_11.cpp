ed82f8e2ac0ccaf4ad09ce963e4ae20bda224ded
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-08-27 14:51:13 +0530

    refactor: Remove unnecessary code for DHT11 temperature and humidity sensor

--- ESP32/src/dht_11.cpp
+++ ESP32/src/dht_11.cpp
@@ -34,17 +34,12 @@ float readDHT() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F\n\n"));
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.println(F("\n\n"));
 
   return t;
 }
