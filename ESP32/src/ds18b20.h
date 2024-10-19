6ac4b763120806d9b798c8d3cae4dac3eaed09e2
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-10-19 14:37:17 +0530

    added more temperature sensors

--- ESP32/src/ds18b20.h
+++ ESP32/src/ds18b20.h
@@ -7,12 +7,18 @@
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 #define ONE_WIRE_BUS1 15
+#define ONE_WIRE_BUS2 16
+#define ONE_WIRE_BUS3 4
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 extern OneWire oneWire;
 extern OneWire oneWire1;
+extern OneWire oneWire2;
+extern OneWire oneWire3;
 extern DallasTemperature sensors;
 extern DallasTemperature sensors1;
+extern DallasTemperature sensors2;
+extern DallasTemperature storageFluid;
 
 void setupDS();
 void readDS();
