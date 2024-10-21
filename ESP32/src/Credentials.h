ee963cb46cb183261b9b447963f218c884b6de22
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-10-21 20:11:57 +0530

    dht test done

--- ESP32/src/Credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-// Replace the next variables with your SSID/Password combination
-#define SSID "Dialog 4G 376"//Add here your Wifi SSID name
-#define WIFI_PASSWORD "Enigmas1" //Add here your Wifi password
-#define MQTT_SERVER "192.168.8.195"// your MQTT broker IPv4 adress - THIS IS DIFFERENT FOR YOU, pls check yours with "ipconfig"  in cmd line
-
-#endif /* CREDENTIALS_H */
